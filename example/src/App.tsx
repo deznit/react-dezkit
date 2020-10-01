52819b72f4242eda7c6b8854d9deeaa686fd9f11
Anoop <deznit.official@gmail.com>
2020-10-01 21:52:56 +0530

    initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-dezkit'
+import 'react-dezkit/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
